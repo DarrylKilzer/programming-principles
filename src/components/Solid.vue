f9119452295d55087f01d952f4a311b350fc2d2e
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2017-05-18 11:29:50 -0600

    first commit

---
+++ src/components/Solid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="solid">
+        <h1>S.O.L.I.D.</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'solid',
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+</style>
